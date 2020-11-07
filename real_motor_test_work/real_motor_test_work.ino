9f260bc107bfa1cb665954dfab2e99d3fd5e6c02
Won Woo Lyu <wlyu125@gmail.com>
2020-11-07 00:17:44 -0600

    real motor work with 0 180 but time seems too long

---
+++ real_motor_test_work/real_motor_test_work.ino
@@ -0,0 +1,31 @@
+//functions from esp32-hal-ledc.h
+//futaba S30003: pulse width: 500-3000 micro seconds
+//1ms=0 degree | 2ms = 180degree while duty cycle is 20ms
+//PWM freq of 5kHZ max of resolution of 13bits: 2^13, 0.012%
+//8bit resolution: 4kHz~8kHz
+//delay(ms)
+
+//GPIO PIN for Servo Control Signal
+const int servo_pin = 1; 
+
+//// setting PWM properties
+const int freq = 4000;
+const int ledChannel = 0;
+const int resolution = 8;
+ 
+void setup(){
+  ledcSetup(ledChannel, freq, resolution);
+  
+  ledcAttachPin(servo_pin, ledChannel);
+}
+ 
+void loop(){
+  for(int pos = 0; pos <= 180; pos++){   
+    ledcWrite(ledChannel, pos);
+    delay(15);
+  }
+  for(int pos = 180; pos >= 0; pos--){
+    ledcWrite(ledChannel, pos);   
+    delay(15);
+  }
+}
--- test_motor/test_motor.ino
+++ test_motor/test_motor.ino
@@ -2,35 +2,43 @@
 
 // create four servo objects 
 Servo servo1;
-
 // Published values for SG90 servos; adjust if needed
 int minUs = 1000;
 int maxUs = 2000;
 
+// These are all GPIO pins on the ESP32
+// Recommended pins include 2,4,12-19,21-23,25-27,32-33
 // for the ESP32-S2 the GPIO pins are 1-21,26,33-42
-int servo1Pin = 1;
+int servo1Pin = 15;
 
 int pos = 0;      // position in degrees
+ESP32PWM pwm;
 void setup() {
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
   Serial.begin(115200);
   servo1.setPeriodHertz(50);      // Standard 50hz servo
 }
 
 void loop() {
   servo1.attach(servo1Pin, minUs, maxUs);
+
+
   for (pos = 0; pos <= 180; pos += 1) { // sweep from 0 degrees to 180 degrees
     // in steps of 1 degree
     servo1.write(pos);
-    Serial.println(servo1.read());
     delay(1);             // waits 20ms for the servo to reach the position
   }
-  Serial.println(getPwm());
   for (pos = 180; pos >= 0; pos -= 1) { // sweep from 180 degrees to 0 degrees
     servo1.write(pos);
-    Serial.println(servo1.read());
     delay(1);
   }
   servo1.detach();
+  pwm.detachPin(27);
+
   delay(5000);
 
 }
